bac6848d8e608bdda8136f8161adc25871b05336
Jana Jezova <jje@odoo.com>
2020-01-27 11:00:50 +0100

    - controller for product categories (up to line 55) => display parent categories and children categories if they exist - manual_pdf and manual_pdf_name on product.product and product.template => to upload the manual in the backend - button on product in the shop view (frontend) for downloading the manual - visible if the manual is imported - frontend view in the shop - display parent categories, then children categories (progressively) and when there is no child category, display products

---
+++ website_adjustment/models/product.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, tools, _
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    manual_pdf = fields.Binary(string='Manual pdf', attachment=True)
+    manual_pdf_name = fields.Char(string='Manual name')
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    manual_pdf = fields.Binary(string='Manual pdf', attachment=True)
+    manual_pdf_name = fields.Char(string='Manual name')
